2b5880b0c1060f1297e99af1d5f6ecc65e632357
kps990515 <31429892+kps990515@users.noreply.github.com>
2021-12-26 03:14:44 +0900

    dd

--- telbot.py
+++ telbot.py
@@ -11,17 +11,32 @@ if __name__ == '__main__':
     token = '5013330145:AAE0WDeo1k0AbCuHO56AgMTk-Aryqi2JGf0'
     bot = telegram.Bot(token=token)
 
+    #step2.Updater(유저의 입력을 계속 모니터링하는 역할), Dispatcher
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    #step3./start 명령어가 입력되었을 때의 함수 정의
+    def s(update, context):
+        context.bot.send_message(chat_id=chat_id, text="작동중!")
+
+    #step4.위에서 정의한 함수를 실행할 CommandHandler 정의
+    start_handler = CommandHandler('s', s) #('명렁어',명령 함수)
+
+    #step5.Dispatcher에 Handler를 추가
+    dispatcher.add_handler(start_handler)
+
+    #step6.Updater 실시간 입력 모니터링 시작(polling 개념)
+    updater.start_polling()
+
     def crawlLp():
         updateLpList = updateLp()
         updateLpNum = len(updateLpList)
-        
-        bot.sendMessage(chat_id=chat_id, text= "작동중") 
-        
+                
         if updateLpNum > 0 :
             for lp in updateLpList:
                 bot.sendMessage(chat_id=chat_id, text= str(lp) + " 추가됨") 
                 
-    schedule.every(5).seconds.do(crawlLp)
+    schedule.every(1).minutes.do(crawlLp)
 
     while True:
         schedule.run_pending()
